992021f35a239ef6e9c136f48a91c6359629b5b1
Alec Brooks <126025860+AlecBrooks@users.noreply.github.com>
2024-11-28 05:30:20 -0800

    update weather r 5 hours

--- R/Weather/Weather.R
+++ R/Weather/Weather.R
@@ -1,146 +1,141 @@
+################################################################################
+                            ### Weather Analysis ###
+################################################################################
+
+                                  ### Setup ###
+################################################################################
 library(readr)
 library(tidyr)
 
-W <- read_csv("New Weather Data - Winter_Data_Final.csv")
-W$DATE <- as.Date(W$DATE, format = "%m/%d/%Y")
+w <- read_csv("Winter_Data_11-28-2024.csv")
+s <- read_csv("Spring_Data_11-28-2024.csv")
+f <- read_csv("Fall_Data_11-28-2024.csv")
 
-#missing Dates
-#2023-02-04
-#2023-02-10
-#2023-02-14
-#2023-02-15
+w$DATE <- as.Date(w$DATE, format = "%m/%d/%Y")
+s$DATE <- as.Date(s$DATE, format = "%m/%d/%Y")
+f$DATE <- as.Date(f$DATE, format = "%m/%d/%Y")
 
-#yearly Time series
-Year_2021 <- subset(W, W$CYCLE == 2021)
-Year_2021$CYCLE_DATE <- 0
-Year_2021$CYCLE_DATE[Year_2021$MONTH == "Dec"] <- as.Date(paste("2000", format(Year_2021$DATE[Year_2021$MONTH == "Dec"], "%m-%d"), sep = "-"))
-Year_2021$CYCLE_DATE[Year_2021$MONTH != "Dec"] <- as.Date(paste("2001", format(Year_2021$DATE[Year_2021$MONTH != "Dec"], "%m-%d"), sep = "-"))
-Year_2021$CYCLE_DATE <- as.Date(Year_2021$CYCLE_DATE, origin = "1970-01-01")
+for (year in 2021:2024) {
+  f_year <- subset(f, f$YEAR == year)
+  w_year <- subset(w, w$YEAR == year)
+  s_year <- subset(s, s$YEAR == year)
+  f_year$CYCLE_DATE <- as.Date(paste("2001", format(f_year$DATE, "%m-%d"), 
+                                     sep = "-"))
+  w_year$CYCLE_DATE <- 0
+  s_year$CYCLE_DATE <- as.Date(paste("2001", format(s_year$DATE, "%m-%d"), 
+                                     sep = "-"))
+  
+  w_year$CYCLE_DATE[w_year$MONTH == "Dec"] <- 
+    as.Date(paste("2000", format(
+      w_year$DATE[w_year$MONTH == "Dec"], "%m-%d"), sep = "-"))
+  
+  w_year$CYCLE_DATE[w_year$MONTH != "Dec"] <- 
+    as.Date(paste("2001", format(
+      w_year$DATE[w_year$MONTH != "Dec"], "%m-%d"), sep = "-"))
+  
+  w_year$CYCLE_DATE <- as.Date(w_year$CYCLE_DATE, origin = "1970-01-01")
+  
+  w_weekly <- aggregate(cbind(SNOW_WC, TEMP_AV) ~ WEEK, data = w_year, mean)
+  w_weekly$CYCLE_WEEK <- c(1:nrow(w_weekly))
+  
+  s_weekly <- aggregate(cbind(SNOW_WC, TEMP_AV) ~ WEEK, data = s_year, mean)
+  s_weekly$CYCLE_WEEK <- c(1:nrow(s_weekly))
+  
+  f_weekly <- aggregate(cbind(SNOW_WC, TEMP_AV) ~ WEEK, data = f_year, mean)
+  f_weekly$CYCLE_WEEK <- c(1:nrow(f_weekly))
+  
+  assign(paste0("f_", year), f_year)
+  assign(paste0("w_", year), w_year)
+  assign(paste0("s_", year), s_year)
+  assign(paste0("w_weekly_", year), w_weekly)
+  assign(paste0("s_weekly_", year), s_weekly)
+  assign(paste0("f_weekly_", year), f_weekly)
+  rm(w_year,f_year,s_year,year,w_weekly,s_weekly,f_weekly)
+}
+colors <- c("#1f78b4", "#33a02c", "#e31a1c", "#ff7f00") 
 
-Year_2022 <- subset(W, W$CYCLE == 2022)
-Year_2022$CYCLE_DATE <- 0
-Year_2022$CYCLE_DATE[Year_2022$MONTH == "Dec"] <- as.Date(paste("2000", format(Year_2022$DATE[Year_2022$MONTH == "Dec"], "%m-%d"), sep = "-"))
-Year_2022$CYCLE_DATE[Year_2022$MONTH != "Dec"] <- as.Date(paste("2001", format(Year_2022$DATE[Year_2022$MONTH != "Dec"], "%m-%d"), sep = "-"))
-Year_2022$CYCLE_DATE <- as.Date(Year_2022$CYCLE_DATE, origin = "1970-01-01")
+                             #### Plots ####
+################################################################################
 
-Year_2023 <- subset(W, W$CYCLE == 2023)
-Year_2023$CYCLE_DATE <- 0
-Year_2023$CYCLE_DATE[Year_2023$MONTH == "Dec"] <- as.Date(paste("2000", format(Year_2023$DATE[Year_2023$MONTH == "Dec"], "%m-%d"), sep = "-"))
-Year_2023$CYCLE_DATE[Year_2023$MONTH != "Dec"] <- as.Date(paste("2001", format(Year_2023$DATE[Year_2023$MONTH != "Dec"], "%m-%d"), sep = "-"))
-Year_2023$CYCLE_DATE <- as.Date(Year_2023$CYCLE_DATE, origin = "1970-01-01")
 
-Year_2024 <- subset(W, W$CYCLE == 2024)
-Year_2024$CYCLE_DATE <- 0
-Year_2024$CYCLE_DATE[Year_2024$MONTH == "Dec"] <- as.Date(paste("2000", format(Year_2024$DATE[Year_2024$MONTH == "Dec"], "%m-%d"), sep = "-"))
-Year_2024$CYCLE_DATE[Year_2024$MONTH != "Dec"] <- as.Date(paste("2001", format(Year_2024$DATE[Year_2024$MONTH != "Dec"], "%m-%d"), sep = "-"))
-Year_2024$CYCLE_DATE <- as.Date(Year_2024$CYCLE_DATE, origin = "1970-01-01")
-
-#############################################################################################
-########### CHARTS #rm(Year_2021, Year_2022, Year_2023, Year_2024, aggregated_data, data_matrix, reshaped_data, FocusDate)
-#############################################################################################
-
-#Cycle Time series Snow
-plot(Year_2021$CYCLE_DATE[-1], Year_2021$SNOW_WC[-1], type = "l", 
+################################# WINTER #######################################
+#Time Series Overlay of Winter Snow
+plot(w_2021$CYCLE_DATE, w_2021$SNOW_WC, type = "l", 
      xlab = "Date (Aligned)", ylab = "Snow Water Content (SNOW_WC)", 
-     main = "Snow Water Content Over Time for All Cycles", col = "blue", lwd = 2,
-     ylim = c(min(Year_2024$SNOW_WC), max(Year_2023$SNOW_WC)))
-lines(Year_2022$CYCLE_DATE, Year_2022$SNOW_WC, col = "red", lwd = 2)
-lines(Year_2023$CYCLE_DATE, Year_2023$SNOW_WC, col = "green", lwd = 2)
-lines(Year_2024$CYCLE_DATE, Year_2024$SNOW_WC, col = "purple", lwd = 2)
+     main = "Winter Snow Water Content 2021 - 2024", col = colors[1], lwd = 2,
+     ylim = c(min(w_2024$SNOW_WC), max(w_2023$SNOW_WC)))
+lines(w_2022$CYCLE_DATE, w_2022$SNOW_WC, col = colors[2], lwd = 2)
+lines(w_2023$CYCLE_DATE, w_2023$SNOW_WC, col = colors[3], lwd = 2)
+lines(w_2024$CYCLE_DATE, w_2024$SNOW_WC, col = colors[4], lwd = 2)
 legend("topleft", legend = c("2021", "2022", "2023", "2024"), 
-       col = c("blue", "red", "green", "purple"), lwd = 2, bty = "n")
-FocusDate <- as.Date("2001-02-05", format = "%Y-%m-%d")
-points(FocusDate, 4, col = "red", pch = 19)
-text(FocusDate, 4, labels = "No Data", pos = 4, col = "red")
+       col = colors, lwd = 2, bty = "n")
 
-# Temperature and Snow Water Content
-plot(Year_2021$CYCLE_DATE[-1], scale(Year_2021$TEMP_AV[-1]), type = "l", 
-     col = "orange", lty = 2, lwd = 2, 
-     xlab = "Date", ylab = "Scaled Values", 
-     main = "Average Temperature and Snow Water Content Over Time (2021)")
-lines(Year_2021$CYCLE_DATE[-1], scale(Year_2021$SNOW_WC[-1]), col = "blue", lwd = 2)
-legend("topright", legend = c("Average Temperature (TEMP_AV)", "Snow Water Content (SNOW_WC)"), 
-       col = c("orange", "blue"), lty = c(2, 1), lwd = 2, bty = "n")
+#Time Series Overlay of Winter Snow
+plot(w_weekly_2021$CYCLE_WEEK, w_weekly_2021$TEMP_AV, type = "l", 
+     xlab = "Date (Aligned)", ylab = "Snow Water Content (TEMP_AV)", 
+     main = "Winter Snow Water Content 2021 - 2024", col = colors[1], lwd = 2,
+     ylim = c(min(w_weekly_2023$TEMP_AV), max(w_weekly_2022$TEMP_AV)))
+lines(w_weekly_2022$CYCLE_WEEK, w_weekly_2022$TEMP_AV, col = colors[2], lwd = 2)
+lines(w_weekly_2023$CYCLE_WEEK, w_weekly_2023$TEMP_AV, col = colors[3], lwd = 2)
+lines(w_weekly_2024$CYCLE_WEEK, w_weekly_2024$TEMP_AV, col = colors[4], lwd = 2)
+legend("topleft", legend = c("2021", "2022", "2023", "2024"), 
+       col = colors, lwd = 2, bty = "n")
 
-#Full Time series
-plot(W$DATE, W$SNOW_WC, type = "l", 
-     xlab = "", ylab = "Snow Water Content over range (SNOW_WC)", main = "Snow Water Content Over Time", col = "blue")
-FocusDate <- as.Date("2023-02-04", format = "%Y-%m-%d")
-points(FocusDate, 4, col = "red", pch = 19)
-text(FocusDate, 4, labels = "No Data", pos = 4, col = "red")
 
-# Seasonal Trends
-aggregated_data <- aggregate(SNOW_WC ~ MONTH + CYCLE, data = W, FUN = mean, na.rm = TRUE)
-reshaped_data <- reshape(aggregated_data, timevar = "CYCLE", idvar = "MONTH", direction = "wide")
+# Temperature to Snow Water Content (scaled)
+for (year in 2021:2024) {
+plot(get(paste0("w_weekly_", year))$CYCLE_WEEK, 
+     scale(get(paste0("w_weekly_", year))$TEMP_AV), type = "l", 
+     col = colors[1], lwd = 2, 
+     xlab = "Week", ylab = "Scaled Values", 
+     main = paste0("Average Weekly TEMP_AV and SNOW_WC", year))
+lines(get(paste0("w_weekly_", year))$CYCLE_WEEK, 
+      scale(get(paste0("w_weekly_", year))$SNOW_WC), col = colors[4],  
+      lty = 2, lwd = 2)
+legend("topleft", legend = c("TEMP_AV", "SNOW_WC"), 
+       col = c(colors[1], colors[4]), lty = c(1, 2), lwd = 2, bty = "n")
+  rm(year)
+}
+
+aggregated_data <- aggregate(SNOW_WC ~ MONTH + YEAR, data = w, 
+                             FUN = mean, na.rm = TRUE)
+reshaped_data <- reshape(aggregated_data, timevar = "YEAR", 
+                         idvar = "MONTH", direction = "wide")
 rownames(reshaped_data) <- reshaped_data$MONTH
 reshaped_data <- reshaped_data[, -1]
 data_matrix <- t(reshaped_data)
-barplot(as.matrix(data_matrix), beside = TRUE, col = rainbow(ncol(data_matrix)), legend.text = c("2021", "2022", "2023", "2024"), args.legend = list(title = "Cycle", x = "topleft"),
-  main = "Grouped Bar Chart of SNOW_WC by Month and Cycle", xlab = "Month", ylab = "Snow Water Content (SNOW_WC)", names.arg = colnames(data_matrix))
+barplot(as.matrix(data_matrix), beside = TRUE, col = 
+          colors, 
+        legend.text = c("2021", "2022", "2023", "2024"), 
+        args.legend = list(title = "Cycle", x = "topleft"),
+        main = "Grouped Bar Chart of SNOW_WC by Month and Cycle", 
+        xlab = "Month", ylab = "Snow Water Content (SNOW_WC)", 
+        names.arg = colnames(data_matrix))
+rm(aggregated_data, data_matrix, reshaped_data)
 
-#Temperature vs. Snow Water Content
-plot(W$TEMP_AV, W$SNOW_WC)
-hist(W$TEMP_AV)
-hist(W$SNOW_WC)
+################################ SPRING ########################################
 
-plot(Year_2021$TEMP_AV, Year_2021$SNOW_WC)
-hist(Year_2021$TEMP_AV)
-hist(Year_2021$SNOW_WC)
-
-summary(lm(W$SNOW_WC ~ W$TEMP_AV))
-summary(lm(Year_2021$SNOW_WC ~ Year_2021$TEMP_AV))
-summary(lm(Year_2022$SNOW_WC ~ Year_2022$TEMP_AV))
-summary(lm(Year_2023$SNOW_WC ~ Year_2023$TEMP_AV))
-summary(lm(Year_2024$SNOW_WC ~ Year_2024$TEMP_AV))
-
-#rm(Year_2021, Year_2022, Year_2023, Year_2024, aggregated_data, data_matrix, reshaped_data, FocusDate)
-
-hist(Year_2021$SNOW_WC)
-hist(Year_2022$SNOW_WC)
-hist(Year_2023$SNOW_WC)
-hist(Year_2024$SNOW_WC)
-
-#anova
-
-x <- aov(TEMP_AV ~ factor(CYCLE), data = W)
-
-summary(x)
-TukeyHSD(x)
-
-W$SNOW_WC
-
-x <- aov(SNOW_WC ~ factor(CYCLE), data = W)
-
-summary(x)
-TukeyHSD(x)
-
-#conf in
-#compare avgs
-#week av for time s of temp
-
-#need poster done by dec 9th
-#remove veg from poster
-#update method to include the new weather day
-#findings need to be updated 1 for flower one for weather
-
-#fall spring weekly ave temp
-#spring: march 10 - May 10
-#fall: aug 10 - oct 10
-
-#---------------------------------
-
-
-
-#Cycle Time series Snow
-plot(Year_2021$CYCLE_DATE[-1], Year_2021$TEMP_AV[-1], type = "l", 
-     xlab = "Date (Aligned)", ylab = "Temp(TEMP_AV)", 
-     main = "TEMP_AV", col = "blue", lwd = 2,
-     ylim = c(min(Year_2024$TEMP_AV), max(Year_2023$TEMP_AV)))
-lines(Year_2022$CYCLE_DATE, Year_2022$TEMP_AV, col = "red", lwd = 2)
-lines(Year_2023$CYCLE_DATE, Year_2023$TEMP_AV, col = "green", lwd = 2)
-lines(Year_2024$CYCLE_DATE, Year_2024$TEMP_AV, col = "purple", lwd = 2)
+plot(s_weekly_2021$CYCLE_WEEK, s_weekly_2021$TEMP_AV, type = "l", 
+     xlab = "Week", ylab = "Weekly Temperature Averages (TEMP_AV)", 
+     main = "Spring Weekly Temperature Averages 2021 - 2024", 
+     col = colors[1], lwd = 2,
+     ylim = c(min(s_weekly_2023$TEMP_AV), max(s_weekly_2021$TEMP_AV)))
+lines(s_weekly_2022$CYCLE_WEEK, s_weekly_2022$TEMP_AV, col = colors[2], lwd = 2)
+lines(s_weekly_2023$CYCLE_WEEK, s_weekly_2023$TEMP_AV, col = colors[3], lwd = 2)
+lines(s_weekly_2024$CYCLE_WEEK, s_weekly_2024$TEMP_AV, col = colors[4], lwd = 2)
 legend("topleft", legend = c("2021", "2022", "2023", "2024"), 
-       col = c("blue", "red", "green", "purple"), lwd = 2, bty = "n")
-FocusDate <- as.Date("2001-02-05", format = "%Y-%m-%d")
-points(FocusDate, 4, col = "red", pch = 19)
-text(FocusDate, 4, labels = "No Data", pos = 4, col = "red")
+       col = colors, lwd = 2, bty = "n")
+
+################################# FALL #########################################
+
+plot(f_weekly_2021$CYCLE_WEEK, f_weekly_2021$TEMP_AV, type = "l", 
+     xlab = "Week", ylab = "Weekly Temperature Averages (TEMP_AV)", 
+     main = "Fall Weekly Temperature Averages 2021 - 2024", 
+     col = colors[1], lwd = 2,
+     ylim = c(min(f_weekly_2021$TEMP_AV), max(f_weekly_2022$TEMP_AV)))
+lines(f_weekly_2022$CYCLE_WEEK, f_weekly_2022$TEMP_AV, col = colors[2], lwd = 2)
+lines(f_weekly_2023$CYCLE_WEEK, f_weekly_2023$TEMP_AV, col = colors[3], lwd = 2)
+lines(f_weekly_2024$CYCLE_WEEK, f_weekly_2024$TEMP_AV, col = colors[4], lwd = 2)
+legend("bottomleft", legend = c("2021", "2022", "2023", "2024"), 
+       col = colors, lwd = 2, bty = "n")
+
